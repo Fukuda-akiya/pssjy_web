0cfc8d36821d9f93b6d337ed3c2fb9d3d23b0da1
Fukuda-akiya <hukuda.akiya.s3@dc.tohoku.ac.jp>
2025-12-25 11:17:22 +0900

    add english page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://github.com',
-  base: 'pssjy_web',
+  site: 'https://fukuda-akiya.github.io',
+  base: '/pssjy_web',
     i18n: {
     defaultLocale: 'jp',
     locales: ['jp', 'en'],
